c3e4b9c0219aec361debf1bf1d5c166c89300e8a
Thomas <thomas.heilbronner@gmail.com>
2018-04-30 20:54:27 +0200

    first failing test to check test setup

---
+++ Java/src/test/java/medicine/PatientTests.java
@@ -0,0 +1,12 @@
+package medicine;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PatientTests {
+
+    @Test
+    void name() {
+        Assertions.fail();
+    }
+}
