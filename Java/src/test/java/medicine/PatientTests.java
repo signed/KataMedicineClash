d949b939e38d6af70700010e7eea961829ec3391
Thomas <thomas.heilbronner@gmail.com>
2018-05-05 23:51:36 +0200

    cleaning up the test code

--- Java/src/test/java/medicine/PatientTests.java
+++ Java/src/test/java/medicine/PatientTests.java
@@ -1,7 +1,6 @@
 package medicine;
 
 import org.junit.jupiter.api.Test;
-import support.MedicineMother;
 import support.PatientBuilder;
 import support.PrescriptionBuilder;
 
@@ -9,8 +8,9 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static support.MedicineMother.Name.Medicine_A;
+import static support.MedicineMother.Name.Medicine_B;
 import static support.MedicineMother.Name.anyMedicineNames;
 import static support.PatientMother.anyDateBeforeTheInspectionPeriod;
 import static support.PatientMother.anyDateWithinTheInspectionPeriod;
@@ -28,16 +28,16 @@ class PatientTests {
     void patient_without_prescription_has_no_clashes() {
         patient = patientWithoutPrescriptions();
 
-        assertThat(queryForClashFor(anyMedicineNames())).isEmpty();
+        assertThat(queryPrescriptionClashesOf(anyMedicineNames())).isEmpty();
     }
 
     @Test
     void querying_for_a_clash_with_only_one_medicine_name_can_not_cause_clashes_even_if_patient_has_a_prescription() {
         PrescriptionBuilder oneDayPrescription = oneDayPrescription().starting(anyDateWithinTheInspectionPeriod());
 
-        patient.withPrescriptionFor(MedicineMother.Name.Medicine_A, oneDayPrescription);
+        patient.withPrescriptionFor(Medicine_A, oneDayPrescription);
 
-        assertThat(queryForClashFor(asList(MedicineMother.Name.Medicine_A))).isEmpty();
+        assertThat(queryPrescriptionClashesOf(Medicine_A)).isEmpty();
     }
 
     @Test
@@ -45,7 +45,7 @@ class PatientTests {
         LocalDate dateOfPrescription = anyDateWithinTheInspectionPeriod();
         thereIsAMedicineClashAt(dateOfPrescription);
 
-        assertThat(queryForClashFor(asList(MedicineMother.Name.Medicine_A, MedicineMother.Name.Medicine_B))).containsOnly(dateOfPrescription);
+        assertThat(queryPrescriptionClashesOf(Medicine_A, Medicine_B)).containsOnly(dateOfPrescription);
     }
 
     @Test
@@ -56,10 +56,10 @@ class PatientTests {
         LocalDate dateOfPrescriptionTwo = anyOtherDateWithinTheInspectionPeriod(dateOfPrescriptionOne);
         PrescriptionBuilder prescriptionTwo = oneDayPrescription().starting(dateOfPrescriptionTwo);
 
-        patient.withPrescriptionFor(MedicineMother.Name.Medicine_A, prescriptionOne);
-        patient.withPrescriptionFor(MedicineMother.Name.Medicine_B, prescriptionTwo);
+        patient.withPrescriptionFor(Medicine_A, prescriptionOne);
+        patient.withPrescriptionFor(Medicine_B, prescriptionTwo);
 
-        assertThat(queryForClashFor(asList(MedicineMother.Name.Medicine_A, MedicineMother.Name.Medicine_B)))
+        assertThat(queryPrescriptionClashesOf(Medicine_A, Medicine_B))
                 .describedAs("prescriptions are on different days and should not clash").isEmpty();
     }
 
@@ -67,21 +67,21 @@ class PatientTests {
     void do_not_report_clashes_that_are_before_the_inspection_period() {
         thereIsAMedicineClashAt(anyDateBeforeTheInspectionPeriod());
 
-        assertThat(queryForClashFor(asList(MedicineMother.Name.Medicine_A, MedicineMother.Name.Medicine_B)))
-                .describedAs("should not be reported as clash because it is before the inspection period").isEmpty();
+        assertThat(queryPrescriptionClashesOf(Medicine_A, Medicine_B))
+                .describedAs("should not be reported as a clash because it is before the inspection period").isEmpty();
     }
 
     private void thereIsAMedicineClashAt(LocalDate clashDay) {
         PrescriptionBuilder oneDayPrescription = oneDayPrescription().starting(clashDay);
-        patient.withPrescriptionFor(MedicineMother.Name.Medicine_A, oneDayPrescription);
-        patient.withPrescriptionFor(MedicineMother.Name.Medicine_B, oneDayPrescription);
+        patient.withPrescriptionFor(Medicine_A, oneDayPrescription);
+        patient.withPrescriptionFor(Medicine_B, oneDayPrescription);
     }
 
-    private Collection<LocalDate> queryForClashFor(String ... medicineNames) {
-        return queryForClashFor(Arrays.asList(medicineNames));
+    private Collection<LocalDate> queryPrescriptionClashesOf(String ... medicineNames) {
+        return queryPrescriptionClashesOf(Arrays.asList(medicineNames));
     }
 
-    private Collection<LocalDate> queryForClashFor(Collection<String> medicineNames) {
+    private Collection<LocalDate> queryPrescriptionClashesOf(Collection<String> medicineNames) {
         return patient.build().clash(medicineNames, daysBack);
     }
 
